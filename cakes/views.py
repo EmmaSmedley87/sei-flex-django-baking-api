77f03986cd9e79f16e42ab480379aba4db25d174
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2021-11-04 20:54:44 +0000

    album migrations and views

--- cakes/views.py
+++ cakes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+from .models import Cakes
 
 # Create your views here.
+
+
+def index(request):
+    # get what we need from the database
+    list = Cakes.objects.all
+# create a context for our object
+    context = {"cakes": list}
+
+# render based on the index .html template
+    return render(request, "cakes/index.html", context)
