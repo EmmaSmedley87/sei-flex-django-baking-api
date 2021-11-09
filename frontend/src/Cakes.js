60987bc1c9fceafaed7eda21d16feb714094b056
Emma Smedley <emmasmedley@Emmas-MacBook-Pro.local>
2021-11-09 21:05:45 +0000

    browser connecting with front end app,word cakes displayed

---
+++ frontend/src/Cakes.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Cakes extends Component {
+  render() {
+    return <div className="Cakes">Cakes</div>;
+  }
+}
+
+export default Cakes;
